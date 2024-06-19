2bdcd6a4d86aa97be9e611d622a0d79133223354
idabrogie <idabrogie@users.noreply.github.com>
2024-06-19 13:05:44 +0000

    deploy: 0f0fd3e15a14c1d7bd645f2b2126ea607e753638

--- src/main/java/com/example/produktapi/exception/EntityNotFoundException.java
+++
@@ -1,8 +0,0 @@
-package com.example.produktapi.exception;
-
-public class EntityNotFoundException extends RuntimeException {
-    public EntityNotFoundException(Integer id) {
-        super(String.format("Produkt med id %d hittades inte", id));
-    }
-}
-
--- src/test/java/com/example/produktapi/features/AddProduct.feature
+++
@@ -1,7 +0,0 @@
-Feature: add product to shopcart
-
-
-  Scenario: As a user add a product to cart
-    Given user clicks on Menu item shop
-    When user clicks on add productButton
-    Then a product should be added to the cart
